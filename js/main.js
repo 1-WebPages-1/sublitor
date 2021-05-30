afb82eb93a5219dfbfc5fa4493a389830237d771
1-WebPages-1 <cabraljorge683@gmail.com>
2021-05-30 01:35:14 -0400

    arreglo, pequeño error js

--- js/main.js
+++ js/main.js
@@ -4,7 +4,6 @@ document.querySelector('.M-Icon-Container').addEventListener('click', () =>
     document.querySelector('.nav-container').classList.toggle('show')
   });
 
- Cookies
 function compruebaAceptaCookies() {
     if(localStorage.aceptaCookies == 'true'){
       cajacookies.style.display = 'none';
